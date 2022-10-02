c7457b39aed50b3aa018c62fe0f82357a4dba8f0
YuliaProkipchuk <96261460+YuliaProkipchuk@users.noreply.github.com>
2022-10-02 12:52:17 +0300

    Update cenzor.component.ts

--- src/app/cenzor/cenzor.component.ts
+++ src/app/cenzor/cenzor.component.ts
@@ -21,11 +21,13 @@ export class CenzorComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  changeColor(color: boolean): void {
-    color ? this.classes = 'redBorder' : this.classes = '';
-  }
-  changeColort(color: boolean): void {
-    color ? this.classest = 'redBorder' : this.classest = '';
+  changeColor(color: boolean, inp: string): void {
+    if (inp == 'input') {
+      color ? this.classes = 'redBorder' : this.classes = '';
+    }
+    else {
+      color ? this.classest = 'redBorder' : this.classest = '';
+    }
   }
   addWord(event: Event): void {
     event.preventDefault();
@@ -33,52 +35,48 @@ export class CenzorComponent implements OnInit {
       let newWord = this.word;
       this.check === 0 ? this.bad += newWord : this.bad += ',' + newWord;
       this.check = 1;
-      console.log(this.word);
       this.prohibited.push(this.word);
 
       this.word = '';
-      this.changeColor(false);
+      this.changeColor(false, 'input');
       this.placeHolder = 'word here...';
     }
     else {
-      this.changeColor(true);
+      this.changeColor(true, 'input');
       this.placeHolder = 'Please write a word!';
     }
 
   }
   resetBtn(): void {
     this.word = '';
-    this.changeColor(false);
+    this.changeColor(false, 'input');
+    this.changeColor(false, 'area');
     this.placeHolder = 'word here...';
+    this.placeHolderArea = 'text here...';
     this.check = 0;
-    this.prohibited.length=0;
-    this.text='';
-    this.bad='';
+    this.prohibited.length = 0;
+    this.text = '';
+    this.bad = '';
   }
   cenzor(e: Event): void {
     e.preventDefault();
     if (!this.text) {
-      this.changeColort(true);
+      this.changeColor(true, 'area');
       this.placeHolderArea = 'Please write a text!';
     }
     else {
-      console.log(this.text);
+      this.changeColor(false, 'area');
+      this.placeHolderArea = 'text here...';
       let index: number;
       let firstHalf: string = '';
       let secondHalf: string = '';
       let val: string = this.text;
-      console.log(val.length);
       let cenzors: string = '';
-      console.log(this.prohibited);
       let k:number;
       for (let i = 0; i < this.prohibited.length; i++) {
         for (k = 0; k < val.length; ) {
           index = val.indexOf(this.prohibited[i]);
-          console.log(index);
-
           if (index > -1) {
-              console.log('yes2 '+val[(index + this.prohibited[i].length)]);
-
               firstHalf = val.slice(0, index);
               secondHalf = val.slice(index + this.prohibited[i].length);
               for (let j = 0; j < this.prohibited[i].length; j++) {
